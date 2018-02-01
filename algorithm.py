4faae50f89cb339bf061d62fed5f0115299b1287
tian <963766613@qq.com>
2018-02-01 22:02:35 +0800

    02-1

---
+++ algorithm.py
@@ -0,0 +1,32 @@
+Machine Learning         机器学习
+Deep Learning            深度学习
+Neural Networks          神经网络
+
+
+1.supervised learning    监督学习(告诉算法每个样本的正确答案)
+2.learning theory        学习理论
+3.unsupervised learning  无监督学习
+4.reinforcement learning 强化学习
+
+
+linear regression    线性回归
+gradient descent     梯度下降(结果依赖参数的初始值)
+the normal equations 正规方程组
+m       样本数量
+x       输入变量/特征
+y       输出变量/目标变量
+(x,y)   一个样本
+θ       参数
+α       学习速度
+:=      把左边变量值设置成右边变量的值，a:=b  b赋值给a
+
+
+RLU     rectified linear unit 修正线性单元
+
+
+图像领域：应用卷积神经网络CNN
+序列数据(音频,语言,字母):应用循环神经网络RNN
+无人驾驶：应用混合的神经网络
+
+结构化数据：输入有着明确清晰的定义
+非结构化数据：音频，图像
---
+++ logistic regression.py
@@ -0,0 +1,3 @@
+logistic regression是一个用于二分分类(binary classification)的算法
+
+X.shape用来输出矩阵的维度  X.shape = (n, m)
