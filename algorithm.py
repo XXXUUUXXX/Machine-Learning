84ef27b030464076f1322f0d9a43833f166dfbd2
tian <963766613@qq.com>
2018-02-02 00:17:27 +0800

    02-2

--- algorithm.py
+++ algorithm.py
@@ -1,12 +1,23 @@
 Machine Learning         机器学习
+    Arthur Samuel定义为机器学习在进行特定编程情况下，给予计算机学习能力的领域
+    Tom Mitchell提出："A computer program is said to learn from experience E with
+    respect to some task T and some performance measure P,if its performance on T,
+    as measured by P, improves with experience E"
+    一个程序被认为能从经验E中学习，解决任务T，达到性能度量值P，当且仅当，有了经验E后，经过P评判，程序在处理T时性能有所提升
+
 Deep Learning            深度学习
 Neural Networks          神经网络
 
 
-1.supervised learning    监督学习(告诉算法每个样本的正确答案)
-2.learning theory        学习理论
-3.unsupervised learning  无监督学习
-4.reinforcement learning 强化学习
+supervised learning    监督学习:教计算机如何去完成任务
+    回归问题：预测连续值的输出(房价问题)
+    分类问题：预测离散值的输出(肿瘤问题01)
+unsupervised learning  无监督学习:让计算机自己进行学习
+learning theory        学习理论
+reinforcement learning 强化学习
+recommender systems    推荐系统
+
+
 
 
 linear regression    线性回归
@@ -24,9 +35,9 @@ y       输出变量/目标变量
 RLU     rectified linear unit 修正线性单元
 
 
-图像领域：应用卷积神经网络CNN
-序列数据(音频,语言,字母):应用循环神经网络RNN
-无人驾驶：应用混合的神经网络
+图像领域：应用的是卷积神经网络CNN
+序列数据(音频,语言,字母):应用的是循环神经网络RNN
+无人驾驶：应用的是混合的神经网络
 
 结构化数据：输入有着明确清晰的定义
 非结构化数据：音频，图像
